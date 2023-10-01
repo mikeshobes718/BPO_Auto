05d202c62b3b7cdb729dc12fb1cf1af2e71a26ce
mikeshobes718 <mikeshobes718@gmail.com>
2023-10-01 19:23:48 -0300

    v0.0.5

--- bpogetter.py
+++ bpogetter.py
@@ -1,80 +1,51 @@
 import imaplib
 import email
-import re
 import datetime
 
 # Your email credentials
 EMAIL = 'careersglobal@unitedinvestmentsfirm.com'
 EMAIL = 'bpo@unitedinvestmentsfirm.com'
-EMAIL = 'bpos@unitedinvestmentsfirm.com'
+# EMAIL = 'bpos@unitedinvestmentsfirm.com'
 PASSWORD = 'Bpos2020'
 IMAP_SERVER = 'mail.unitedinvestmentsfirm.com'
 
+# Print your personal email and host
+print(f"Customer Email: {EMAIL}")
+host = IMAP_SERVER.split("mail.")[-1]
+print(f"Host: {host}\n")
+
 # Connect to your mailbox using IMAP
-try:
-    mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-    mail.login(EMAIL, PASSWORD)
-    mail.select('inbox')
-except imaplib.IMAP4.error as e:
-    if "AUTHENTICATIONFAILED" in str(e):
-        print("Authentication failed. Please check your email and password.")
-    else:
-        print(f"An IMAP error occurred: {e}")
-    exit(1)
-except ConnectionRefusedError:
-    print("Error: Unable to connect to the IMAP server. Please check the server address and port.")
-    exit(1)
+mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+mail.login(EMAIL, PASSWORD)
+mail.select('inbox')
 
-# Search for emails from the past 2 days, including today
-two_days_ago = (datetime.datetime.now() - datetime.timedelta(days=365)).strftime('%d-%b-%Y')
-status, email_ids = mail.search(None, '(SINCE "' + two_days_ago + '")')
+# Search for emails from the past 3 days
+three_days_ago = (datetime.datetime.now() - datetime.timedelta(days=3)).strftime('%d-%b-%Y')
+status, email_ids = mail.search(None, '(SINCE "' + three_days_ago + '")')
 
-# Debug print: Print the status and number of email IDs retrieved
-print(f"Search status: {status}")
-print(f"Number of emails retrieved: {len(email_ids[0].split())}")
+# Keywords to search for in the email subject
+subject_keywords = ["bid", "request", "assignment"]
 
-# Check if there are no emails from the past 2 days
-if not email_ids or not email_ids[0]:
-    print("No emails found from the past 2 days.")
-    exit(0)
+# Counter for emails that match the criteria
+counter = 0
 
-# Keywords and regex patterns to search for
-keywords = ["bpo", "fee", "Bid", "assignment"]
-fee_bid_pattern = re.compile(r'(fee|bid):\s*\$\s*(\d+(\.\d{2})?)', re.IGNORECASE)
-
-# Iterate through all emails and add a debug print for each email processed
+# Iterate through all emails
 for e_id in email_ids[0].split():
-    print(f"Processing email ID: {e_id.decode('utf-8')}")
-    
     status, email_data = mail.fetch(e_id, '(RFC822)')
     raw_email = email_data[0][1]
 
     # Parse the raw email
     msg = email.message_from_bytes(raw_email)
+    email_subject = msg['subject']
     email_from = msg['from']
     email_date = msg['date']
-    email_body = ""
 
-    if msg.is_multipart():
-        for part in msg.walk():
-            content_type = part.get_content_type()
-            content_disposition = str(part.get("Content-Disposition"))
-            
-            match content_type:
-                case "text/plain" if "attachment" not in content_disposition:
-                    try:
-                        body = part.get_payload(decode=True).decode('utf-8', errors='replace')
-                        email_body += body
-                    except:
-                        pass
-    else:
-        email_body += msg.get_payload(decode=True).decode('utf-8', errors='replace')
-
-    # Check if the email body contains fee or bid amount
-    match = fee_bid_pattern.search(email_body)
-    if match:
-        amount_type = match.group(1)
-        amount = match.group(2)
+    # Check if the email subject contains the keywords
+    if any(keyword.lower() in email_subject.lower() for keyword in subject_keywords):
+        counter += 1
+        print(f"Email {counter}:")
         print(f"From: {email_from}")
         print(f"Date Sent: {email_date}")
-        print(f"{amount_type.capitalize()}: ${amount}\n\n")
+        print("-----------\n")
+
+print(f"Total emails found with the specified keywords: {counter}")
