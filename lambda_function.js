91bd8f14b69755b4df85a8ae00163313b2abe422
mikeshobes718 <mikeshobes718@gmail.com>
2023-11-12 22:21:17 -0300

    gets the user name and password of who the order belongs to
    gets the user name and password of who the order belongs to

--- lambda_function.js
+++ lambda_function.js
@@ -35,6 +35,26 @@ exports.handler = async (event) => {
         const credentials = await getGoogleCredentials();
         await authenticateGoogleSheets(credentials);
 
+        // Log the name of the file that triggered the Lambda function
+        const bucketName = event.Records[0].s3.bucket.name;
+        const fileKey = event.Records[0].s3.object.key;
+        console.log("Triggered by the file: " + fileKey);
+
+        // Retrieve the actual data from the S3 object that triggered the Lambda function
+        const s3Params = {
+            Bucket: bucketName,
+            Key: fileKey
+        };
+        const s3Data = await s3.getObject(s3Params).promise();
+        const fileDataString = s3Data.Body.toString('utf-8');
+        const fileData = JSON.parse(fileDataString); // Parse the JSON string into an object
+
+        // Extract the From and To values
+        const fromValue = fileData.From || 'Not available';
+        const toValue = fileData.To || 'Not available';
+        console.log("From:", fromValue);
+        console.log("To:", toValue);
+
         // Specify the Google Sheet ID and the range you want to read from
         const spreadsheetId = '1Eko1zD65X0XntfyTYFYmX6vQGEFjWu_3UvszKRoNSPE'; // The provided Google Sheet ID
         const range = 'Sheet1!A2:C2'; // Accesses the first row of data in 'Sheet1'
@@ -49,11 +69,13 @@ exports.handler = async (event) => {
 
         // Construct the response body with the retrieved data
         const responseBody = {
-            message: 'First row data from Google Sheet retrieved successfully',
-            firstRowData: firstRowData // Include the first row data from Google Sheet in the response
+            message: 'First row data from Google Sheet and S3 file data retrieved successfully',
+            firstRowData: firstRowData, // Include the first row data from Google Sheet in the response
+            from: fromValue, // The "From" value extracted from fileData
+            to: toValue // The "To" value extracted from fileData
         };
 
-        // Return a successful response containing the first row data
+        // Return a successful response containing only the 'from' and 'to' values
         return {
             statusCode: 200,
             body: JSON.stringify(responseBody, null, 2)
